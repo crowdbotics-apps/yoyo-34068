7c8d74710e47d8e606ed7c1821b0b08175e5d737
Crowdbotics <team@crowdbotics.com>
2022-03-09 11:41:56 +0000

    Initial commit.

---
+++ backend/yoyo_34068/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for yoyo_34068 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yoyo_34068.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "yoyo_34068",
+  url: "https://yoyo_34068.botics.co",
+  api: "https://yoyo_34068.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
